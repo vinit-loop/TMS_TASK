020c93388caabf99c964cc0bfc17ca772c1bdacf
vinit <vinitkukadiya.work1@gmail.com>
2025-02-21 00:16:32 +0530

    initial commit

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,30 @@
-  import express from "express";
-  import mongoose from "mongoose";
-  import userRouter from "./routes/user/UserRoute.js";
+import express from "express";
+import mongoose from "mongoose";
+import userRouter from "./routes/user/UserRoute.js";
 
-  import cors from "cors";
-  import authRoutes from "./routes/authRoute.js";
-  import CategoryRouter from "./routes/category/CategoryRoutes.js";
+import cors from "cors";
+import authRoutes from "./routes/authRoute.js";
+import CategoryRouter from "./routes/category/CategoryRoutes.js";
 
-  const app = express();
+const app = express();
 
-  app.use(cors());
-  app.use(express.json());
-  app.use("/auth", authRoutes);
-  app.use("/", userRouter);
+app.use(cors());
+app.use(express.json());
+app.use("/auth", authRoutes);
+app.use("/", userRouter);
 
-  app.use("/api/category", CategoryRouter);
-  
-  
+app.use("/api/category", CategoryRouter);
 
-  const connect = async () => {
-    try {
-      await mongoose.connect("mongodb://localhost:27017/MCM");
-      console.log("success");
-    } catch (error) {
-      console.log("unable to connect");
-    }
-  };
-  connect();
+const connect = async () => {
+  try {
+    await mongoose.connect("mongodb://localhost:27017/MCM");
+    console.log("success");
+  } catch (error) {
+    console.log("unable to connect");
+  }
+};
+connect();
 
+app.listen(8000, () => console.log("Server running on port 8000"));
 
-  app.listen(8000, () => console.log("Server running on port 8000"));
-
-  export default app;
+export default app;
